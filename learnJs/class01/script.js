0daf0d19727ab903e0ad692073a8163f7f73fbcb
Adnan <connect.adnan.guru@gmail.com>
2021-11-14 18:13:20 +0500

    js class01

---
+++ learnJs/class01/script.js
@@ -0,0 +1,42 @@
+ 
+
+ var age = 10
+ var num1 = 100
+ var name = "naveed"
+ console.log(age);
+ 
+//  date types
+// int age = 10
+// float age1 = 12.2
+// bool flag = false
+// flex
+
+var a = 'a'
+// var b = 10
+// var result = a + b
+// console.log("result", result);
+// var result = 5 + 10 > 15
+// var result = 15
+
+console.log("result of firstProgram", a);
+
+// program1
+// write a js program, define two variables and sum those 
+// two variable and save result in third variable and print in console?
+
+// program2
+// write a js program, that can divide two numbers and show 
+// the result in console?
+console.log("result of firstProgram", a);
+
+// program3
+// write a js program, that can add two number, divide result
+//  by 5 and show in console?
+console.log("result of firstProgram", a);
+
+// program4
+// write a js program, that can convert temp from F into C and show into console, 
+// using C = (F − 32) * 5/9 
+// use if F = 50, what will be the value of C? 
+
+console.log("Divide two number°", a);
