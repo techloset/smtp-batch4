3b4ac3a39c666c006149820946e69394b8fe340a
Adnan <connect.adnan.guru@gmail.com>
2021-11-27 19:59:13 +0500

    js class 05

---
+++ learnJs/class04/script.js
@@ -0,0 +1,53 @@
+// nested if else
+// if (condition) {
+//     if (condition) {
+//         if (condition) {
+//            if (condition) {
+
+//            }
+//         }
+//     }
+// }
+
+// if (2 === 2) {
+//     if (2<5) {
+//         console.log("statement is correct");
+//     }
+// }
+
+// if else if
+if ( 5 < 3) {
+    
+}
+else if(3> 3){
+
+}
+else if(1 < 2){
+
+}
+else{
+
+}
+
+// Problem Solving
+// Write a js program to find maximum between two numbers(take input from user) using conditional operator.
+// 2 , 4
+
+var num1 = parseInt(prompt("Enter First Number"));
+var num2 = parseInt(prompt("Enter second Number"));
+
+var flag = num1 > num2;
+if (num1 === num2) {
+  console.log("Numbers are equal", num1, num2);
+} else {
+  if (flag) {
+    console.log("Num1 is greater than number two", num1);
+  } else {
+    console.log("Num2 is greater than number one", num2);
+  }
+}
+
+
+// 2. Write a js program to find maximum between three numbers using conditional operator.
+// 3. write a js program, to check weather number is int or not
+// 4. write a js program, to check weather number is string of not
---
+++ learnJs/class05/script.js
@@ -0,0 +1,63 @@
+// write a js program, add 5 and 10 two numbers and show the result in console
+
+console.log("Line no.2");
+function addTwoNumbers(num1 = 5, num2 = 10) {
+  console.log("Param1 ", num1);
+  console.log("Param2 ", num2);
+  // var result = num1 + num2;
+  // console.log("result - line no.7", result);
+  // console.log("result - line no.7", result);
+  // console.log("result - line no.7", result);
+  // console.log("result - line no.7", result);
+  // console.log("result - line no.7", result);
+  // console.log("result - line no.7", result);
+}
+parseInt()
+
+// function addTwoNumbers2() {
+//   var num1 = 15;
+//   var num2 = 10;
+//   var result = num1 + num2;
+//   console.log("result - line no.7", result);
+// }
+console.log("Line no.9");
+// var number = prompt("Enter Number",3,5)
+// addTwoNumbers(number,10);
+
+
+
+console.log("line14");
+
+
+// addTwoNumbers();
+addTwoNumbers();
+// addTwoNumbers(15,17);
+// if (condition) {
+//   var num1 = 5;
+//   var num2 = 10;
+//   var result = num1 + num2;
+//   console.log("result", result);
+// }
+
+// functions
+
+// write a js program, add 10 and 15 two numbers and show the result in console
+// var num3 = 10;
+// var num4 = 15;
+// var result2 = num3 + num4;
+// console.log("result", result2);
+
+
+// DOM
+
+function setH2Text() {
+var dom = document;
+var someName = dom.getElementById("idH2")
+console.log("someName", someName.innerHTML);
+someName.innerHTML = "Some New Text from js"
+console.log("dom", dom);
+}
+
+// setH2Text()
+
+
