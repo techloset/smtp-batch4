b428131e7c4135f2eadba4739350e87fa88b4202
naveed-rana <rana.naveed812@gmail.com>
2022-03-06 20:11:52 +0500

    class 07

--- ReactJS/class-07/src/App.js
+++ ReactJS/class-07/src/App.js
@@ -1,187 +1,81 @@
-import React, { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid';
-
-import './App.css'
-const data = [
-
-  {
-    uid: uuidv4(),
-    name: 'naveed',
-    email: 'learnwithnaveedsarwar@gmail.com',
-    rollNo: 234234,
-    className: 'web and mobile'
-  },
-
-
-  {
-    uid: uuidv4(),
-    name: 'Umar',
-    email: 'umar@gmail.com',
-    rollNo: 2342002334,
-    className: 'web and mobile'
-  },
-
-
-  {
-    uid: uuidv4(),
-    name: 'Ali',
-    email: 'Ali@gmail.com',
-    rollNo: 23,
-    className: 'web and mobile'
-  },
-
-
-
-
-]
+import React,{useState} from 'react'
 
 export default function App() {
-
+  
+  const [email, setEmail] = useState('learnwithnaveedsarwar@gmail.com')
   const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('')
-  const [rollNo, setRollNo] = useState('')
-  const [className, setClassName] = useState('')
 
-  const [students, setStudents] = useState(data)
+  const [students, setStudents] = useState([1,2,3,4,5])
+ const [flag, setFlag] = useState(true)
 
 
-  const [isUpdate, setIsUpdate] = useState(false)
-  const [updateUid, setUpdateUid] = useState(0)
+ const onClickHandler = () =>{
 
+  // repeat js
 
+  // for loop
 
-  const onChangeNameHandler = (e) => {
-    console.log('event---', e.target.value);
-    setUserName(e.target.value)
+  for (let index = 0; index < 10; index++) {
+   console.log('for loop', index);
   }
 
-  const onSubmitHandler = () => {
-    console.log('submit button clicked');
-    if (!userName || !email || !rollNo || !className) {
-      alert('All inputs are required')
-      return
-    }
+  // while loop
 
-    // do some logics with the form data
-    console.log('Values', userName, email, rollNo, className)
-
-    let student = {
-      uid: uuidv4(),
-      name: userName,
-      email: email,
-      rollNo: rollNo,
-      className: className
-    }
-
-    setStudents([...students, student])
-
-    setClassName('')
-    setUserName('')
-    setRollNo('')
-    setEmail('')
-  }
-
-  const onDeleteHandler = (uid) => {
-
-    console.log('email', uid);
-
-    let newStudents = students.filter((student) => student.uid !== uid)
-
-    setStudents(newStudents)
-
-  }
-
-  const onUpdateHandler = (item, index) => {
-    setClassName(item.className)
-    setUserName(item.name)
-    setRollNo(item.rollNo)
-    setEmail(item.email)
-    setUpdateUid(item)
-    setIsUpdate(true)
-  }
-
-  const onCtaUpdate = () => {
-    console.log('onCtaUpdate');
-
-    let student = {
-      name: userName,
-      email: email,
-      rollNo: rollNo,
-      className: className
-    }
+  // do while lopp
 
 
-    let newStudents = students.map((item,index) => {
-      if (item.uid ===  updateUid) {
-        return student
-      }
-      else {
-        return item
-      }
-    })
+  // for each loop
+  var returnForEach = students.forEach(element => {
+    console.log('element', element);
+  });
 
-    setStudents(newStudents)
+console.log("returnForEach", returnForEach);
+  // filter loop
+  var returnFilterLoop = students.filter(element => {
+    console.log('element', element);
+    return element * 2
+  });
+console.log("returnFilterLoop", returnFilterLoop);
 
-    setClassName('')
-    setUserName('')
-    setRollNo('')
-    setEmail('')
-    setIsUpdate(false)
+  // map loop
 
+  var returnMapLoop = students.map(element => {
+    console.log('element', element);
+    return element
+  });
+console.log("returnMapLoop", returnMapLoop);
 
+ 
 
-  }
+ }
+
+//  const ctaHanlder = ()=>{
+//   onClickHandler()
+//  }
+ 
+
+const onChangeHandler = (e) =>{
+  console.log("e", e.target.name);
+  // setUserName()
+
+}
+
 
 
   return (
     <div>
-      <h1>Student SignUp Form</h1>
-      <div>Name: <input onChange={(e) => setUserName(e.target.value)} value={userName} placeholder='enter your name' type='text' /> </div>
-      <div>Email: <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder='enter your Email' type='text' /> </div>
-      <div>Roll No.: <input onChange={(e) => setRollNo(e.target.value)} value={rollNo} placeholder='enter your Roll No.' type='text' /> </div>
-      <div>Class: <input onChange={(e) => setClassName(e.target.value)} value={className} placeholder='enter your Class' type='text' /> </div>
-      {isUpdate ?
-        <button onClick={onCtaUpdate}>Update</button> :
-        <button onClick={onSubmitHandler}>Submit</button>
-      }
 
+    {flag?  <h1>Learn Js</h1> : ""}
+   
+    <p>email:{email}</p>
 
-      <h1>List of Students</h1>
+    <p>name:{userName}</p>
 
-      <table >
-        <tr className='customTable'>
-          <th>No.</th>
-          <th>UID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Roll No.</th>
-          <th>Class</th>
-          <th>Actions</th>
-        </tr>
+    {students.map((item,index)=> <h1>Hello {item} index{index} </h1>)}
 
-        {
-          students.map((item, index) => {
-            return (
-              <tr key={index} className='customTable'>
-                <td>{index + 1}</td>
-                <td>{item.uid}</td>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.rollNo}</td>
-                <td>{item.className}</td>
-                <td>
-                  <button onClick={() => onDeleteHandler(item.uid)}>Delete</button>
-                  <button onClick={() => onUpdateHandler(item, index)}>Update</button>
-                </td>
-              </tr>
-            )
-          })
-        }
-
-
-      </table>
-
-
+    <input type="text" name="userName" onChange={(e)=>onChangeHandler(e)} />
+    
+    <button required onClick={()=>onClickHandler()}>submit</button>
 
     </div>
   )
